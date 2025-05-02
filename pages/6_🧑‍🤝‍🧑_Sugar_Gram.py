182c52fd7a6d78f483079385203579388fe8ecf7
Carl Mitchell <griseldacarl@gmail.com>
2025-05-02 14:41:20 -0700

    Changed to link for DigitalOcean server

--- pages/6_🧑‍🤝‍🧑_Sugar_Gram.py
+++ pages/6_🧑‍🤝‍🧑_Sugar_Gram.py
@@ -1,6 +1,7 @@
-import streamlit as st
 import os
+
+import streamlit as st
 
 st.set_page_config("Sugar Grame", page_icon=":material/share:", layout="centered")
 os.environ.get("BASE_API_URL")
-st.page_link("http://192.168.6.176:3001",label="SugarGram",icon=":material/share:")
+st.page_link("http://147.182.203.196:3001", label="SugarGram", icon=":material/share:")
